7966610254defec995074323534b00797e5b8119
liuchaoqun01 <liuchaoqun01@baidu.com>
2018-02-05 19:25:58 +0800

    spider

---
+++ SpiderPrivate/NewsSpider/NewsSpider.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+import urllib2
+import requests
+import re
+from lxml import etree
+
+
+def StringListSave(save_path, filename, slist):
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    path = save_path+"/"+filename+".txt"
+    with open(path, "w+") as fp:
+        for s in slist:
+            fp.write("%s\t\t%s\n" % (s[0].encode("utf8"), s[1].encode("utf8")))
+    return
+
+def Page_Info(myPage):
+    '''Regex'''
+    mypage_Info = re.findall(r'<div class="titleBar" id=".*?"><h2>(.*?)</h2><div class="more"><a href="(.*?)">.*?</a></div></div>', myPage, re.S)
+    return mypage_Info
+
+def New_Page_Info(new_page):
+    '''Regex(slowly) or Xpath(fast)'''
+    dom = etree.HTML(new_page)
+    new_items = dom.xpath('//tr/td/a/text()')
+    new_urls = dom.xpath('//tr/td/a/@href')
+    assert (len(new_items) == len(new_urls))
+    return zip(new_items, new_urls)
+
+def Spider(url):
+    i = 0
+    print 'download: ', url
+    page_content = requests.get(url).content.decode('gbk')
+    myPageResults = Page_Info(page_content)
+    save_path = u"网易新闻抓取"
+    filename = str(i) + '_' + u"新闻排行榜"
+    StringListSave(save_path, filename, myPageResults)
+    i += 1
+    for item, url in myPageResults:
+        print "downloading ", url
+        new_pages = requests.get(url).content.decode('gbk')
+        new_pages_info = New_Page_Info(new_pages)
+        filename = str(i) + "_" + item
+        StringListSave(save_path, filename, new_pages_info)
+        i += 1
+    return
+
+if __name__ == '__main__':
+    print "start"
+    start_url = "http://news.163.com/rank/"
+    Spider(start_url)
+
+    print "end"
